f4a36ba4defabcd315a671e6eaca529d115dee3a
RyanHuang <neverforever1201@gmail.com>
2021-12-01 15:44:58 -0500

    New OptUnchokeNeighbor - v1

--- src/peer/OptimisticallyUnchockedNeighbors.java
+++ src/peer/OptimisticallyUnchockedNeighbors.java
@@ -11,64 +11,40 @@ import java.util.Set;
 import java.util.TimerTask;
 import java.util.Vector;
 
-/**
- * This class is used to determine optimistically unchoked neighbor from a list of choked neighbors
- */
 public class OptimisticallyUnchockedNeighbors extends TimerTask {
 
-    /**
-     * This method runs asynchronously as part of timer task every 'CommonConfiguration.optimisticUnchokingInterval' interval.
-     * It determines optimistically unchoked neighbor at random from all the neighbors which are choked.
-     */
     @Override
     public void run() {
         peerProcess.updateOtherPeerDetails();
-        if (!peerProcess.optimisticUnchokedNeighbors.isEmpty()) {
-            peerProcess.optimisticUnchokedNeighbors.clear();
-        }
+        peerProcess.optimisticUnchokedNeighbors.clear();
 
-        //Add all interested peers to a list
-        Set<String> keys = peerProcess.remotePeerDetailsMap.keySet();
-        Vector<RemotePeerDetails> remotePeerDetailsVector = new Vector();
-        for (String key : keys) {
-            RemotePeerDetails remotePeerDetails = peerProcess.remotePeerDetailsMap.get(key);
-            if (!key.equals(peerProcess.currentPeerID) && hasPeerInterested(remotePeerDetails)) {
-                remotePeerDetailsVector.add(remotePeerDetails);
+        List<RemotePeerDetails> interestedPeerDetailsInArray = new ArrayList();
+        for (RemotePeerDetails peerDetailObject : peerProcess.remotePeerDetailsMap.values()){
+            if (peerDetailObject.getId().equals(peerProcess.currentPeerID))
+                continue;
+            else if (  peerDetailObject.getIsComplete()==0
+                    && peerDetailObject.getIsChoked()==1
+                    && peerDetailObject.getInterested()==1){
+                interestedPeerDetailsInArray.add(peerDetailObject);
             }
         }
 
-        if(!remotePeerDetailsVector.isEmpty()) {
-            //randomize the list and get the first element from it.
-            Collections.shuffle(remotePeerDetailsVector);
-            RemotePeerDetails remotePeerDetails = remotePeerDetailsVector.firstElement();
-            peerProcess.optimisticUnchokedNeighbors.put(remotePeerDetails.getId(), remotePeerDetails);
-            logAndShowInConsole(peerProcess.currentPeerID + " has the optimistically unchoked neighbor " + remotePeerDetails.getId());
+        if(interestedPeerDetailsInArray.size()>0) {
+            Collections.shuffle(interestedPeerDetailsInArray);
+            RemotePeerDetails selectedPeerDeatilObject = interestedPeerDetailsInArray.firstElement();
+            String selectedPeerId = selectedPeerDeatilObject.getId();
+            peerProcess.optimisticUnchokedNeighbors.put(selectedPeerId, selectedPeerDeatilObject);
+            logAndShowInConsole(peerProcess.currentPeerID + " makes the optimistically unchoked neighbor " + selectedPeerId + " now.");
 
-            if(remotePeerDetails.getIsChoked() == 1) {
-                //send unchoke message if choked
-                peerProcess.remotePeerDetailsMap.get(remotePeerDetails.getId()).setIsChoked(0);
-                sendUnChokedMessage(peerProcess.peerToSocketMap.get(remotePeerDetails.getId()), remotePeerDetails.getId());
-                sendHaveMessage(peerProcess.peerToSocketMap.get(remotePeerDetails.getId()), remotePeerDetails.getId());
-                peerProcess.remotePeerDetailsMap.get(remotePeerDetails.getId()).setPeerState(3);
+            if(selectedPeerDeatilObject.getIsChoked() == 1) {
+                selectedPeerDeatilObject.setIsChoked(0);
+                sendUnChokedMessage(peerProcess.peerToSocketMap.get(selectedPeerId), selectedPeerId);
+                sendHaveMessage(peerProcess.peerToSocketMap.get(selectedPeerId), selectedPeerId);
+                selectedPeerDeatilObject.setPeerState(3);
             }
         }
     }
 
-    /**
-     * This method is used to determine if the peer is interested to receive pieces
-     * @param remotePeerDetails - peer to check whether it is interested or not
-     * @return true - peer interested; false - peer not interested
-     */
-    private boolean hasPeerInterested(RemotePeerDetails remotePeerDetails) {
-        return remotePeerDetails.getIsComplete() == 0 &&
-                remotePeerDetails.getIsChoked() == 1 && remotePeerDetails.getIsInterested() == 1;
-    }
-
-    /**
-     * This method is used to send UNCHOKED message to socket
-     * @param socket - socket in which the message to be sent
-     * @param remotePeerID - peerID to which the message should be sent
-     */
     private void sendUnChokedMessage(Socket socket, String remotePeerID) {
         logAndShowInConsole(peerProcess.currentPeerID + " sending a UNCHOKE message to Peer " + remotePeerID);
         Message message = new Message(Message.MessageConstants.MESSAGE_UNCHOKE);
@@ -76,11 +52,6 @@ public class OptimisticallyUnchockedNeighbors extends TimerTask {
         SendMessageToSocket(socket, messageInBytes);
     }
 
-    /**
-     * This method is used to send HAVE message to socket
-     * @param socket - socket in which the message to be sent
-     * @param peerID - peerID to which the message should be sent
-     */
     private void sendHaveMessage(Socket socket, String peerID) {
         logAndShowInConsole(peerProcess.currentPeerID + " sending HAVE message to Peer " + peerID);
         byte[] bitFieldInBytes = peerProcess.bitFieldMessage.getBytes();
@@ -88,11 +59,6 @@ public class OptimisticallyUnchockedNeighbors extends TimerTask {
         SendMessageToSocket(socket, Message.convertMessageToByteArray(message));
     }
 
-    /**
-     * This method is used to write a message to socket
-     * @param socket - socket in which the message to be sent
-     * @param messageInBytes - message to be sent
-     */
     private void SendMessageToSocket(Socket socket, byte[] messageInBytes) {
         try {
             OutputStream out = socket.getOutputStream();
@@ -101,10 +67,6 @@ public class OptimisticallyUnchockedNeighbors extends TimerTask {
         }
     }
 
-    /**
-     * This method is used to log a message in a log file and show it in console
-     * @param message - message to be logged and showed in console
-     */
     private static void logAndShowInConsole(String message) {
         LogHelper.logAndPrint(message);
     }
